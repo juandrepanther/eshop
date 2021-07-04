908d1021bfe6b912b0636081a0b120a81b4aa237
juandrepanther <janis@innergang.com>
2021-07-04 17:57:03 +0300

    formated code Prettier

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,39 @@
-import {Component} from 'react'
+import { Component } from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Category from './components/Category'
 import NavBar from './components/NavBar'
 import './styles/App.css'
 //Apollo imports
 import ApolloClient from 'apollo-boost'
-import {ApolloProvider} from 'react-apollo'
+import { ApolloProvider } from 'react-apollo'
 //Redux imports
-import {Provider} from 'react-redux'
-import {store} from './redux/store'
+import { Provider } from 'react-redux'
+import { store } from './redux/store'
 
 //Apollo Setup
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/'
+  uri: 'http://localhost:4000/',
 })
 
 //Class main render
 class App extends Component {
-  
   render() {
-    
     return (
       <BrowserRouter>
         <Provider store={store}>
-        <ApolloProvider client={client}>
-          <NavBar />
-          <Switch>
-            <Route exact path='/clothes' component={Category}/>
-            <Route exact path='/tech' component={Category}/>
-          </Switch>
-        </ApolloProvider>
+          <ApolloProvider client={client}>
+            <NavBar />
+
+            <Switch>
+              <Route exact path="/clothes" component={Category} />
+              <Route exact path="/tech" component={Category} />
+            </Switch>
+          </ApolloProvider>
         </Provider>
       </BrowserRouter>
     )
   }
 }
 
-export default App;
+export default App
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,6 +1,6 @@
-import {PureComponent} from 'react'
-import {connect} from 'react-redux'
-import { addValue } from "../redux/rootReducers";
+import { PureComponent } from 'react'
+import { connect } from 'react-redux'
+import { addValue } from '../redux/rootReducers'
 
 //needed for multiple default export at the end of component
 
@@ -8,83 +8,85 @@ import '../styles/NavBar.css'
 import Logo from '../media/Logo.png'
 import Basket from '../media/Basket.png'
 
-import {NavLink} from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 class NavBar extends PureComponent {
-    constructor(props) {
-        super(props)
-        this.state = {
-            value: ''
-        }
-        this.handleChange = this.handleChange.bind(this)
-        this.update = this.update.bind(this)
-        
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: '',
     }
-        handleChange = (e) => {
-            console.log(e.target.value);
-        this.setState({
-            value: e.target.value
-        })   
-        }
+    this.handleChange = this.handleChange.bind(this)
+    this.update = this.update.bind(this)
+  }
+  handleChange = (e) => {
+    console.log(e.target.value)
+    this.setState({
+      value: e.target.value,
+    })
+  }
 
-        update = () => {
-            const {addValue} = this.props
-            addValue(this.state.value)
-        }
+  update = () => {
+    const { addValue } = this.props
+    addValue(this.state.value)
+  }
 
-    render() {
-        console.log(this.props);
-        return (
-          <>
-           <div className='navbar-container'>
-               <div className="navbar-wrapper">
-               <div className='navbar-container-one'>
-                   <ul className='nav-menu'>
-                        <NavLink to='/clothes'><li className='nav-item' value='clothes'>CLOTHES</li></NavLink>
-                        <NavLink to='/tech'><li className='nav-item' value='tech'>TECH</li></NavLink>
-                   </ul>
-               </div>
-               <div className='navbar-container-two'>
-                   <img src={Logo} alt="" className="logo" />
-                </div>
-                <div className="navbar-container-three">
-                    <select className="currency">
-                        <option value="1" className="option"></option>
-                        <option value="2" className="option"></option>
-                        <option value="3" className="option"></option>
-                        <option value="4" className="option"></option>
-                    </select>
-                    <img src={Basket} alt="" className="basket" />
-                </div>
-                </div>
+  render() {
+    console.log(this.props)
+    return (
+      <>
+        <div className="navbar-container">
+          <div className="navbar-wrapper">
+            <div className="navbar-container-one">
+              <ul className="nav-menu">
+                <NavLink to="/clothes">
+                  <li className="nav-item" value="clothes">
+                    CLOTHES
+                  </li>
+                </NavLink>
+                <NavLink to="/tech">
+                  <li className="nav-item" value="tech">
+                    TECH
+                  </li>
+                </NavLink>
+              </ul>
+            </div>
+            <div className="navbar-container-two">
+              <img src={Logo} alt="" className="logo" />
+            </div>
+            <div className="navbar-container-three">
+              <select className="currency">
+                <option value="1" className="option"></option>
+                <option value="2" className="option"></option>
+                <option value="3" className="option"></option>
+                <option value="4" className="option"></option>
+              </select>
+              <img src={Basket} alt="" className="basket" />
+            </div>
+          </div>
 
-
-                <div>
-        <input type='text' value={this.state.value} onChange={this.handleChange}></input>
-        
-        <button onClick={this.update}>
-          Update
-        </button>
-        
+          <div>
+            <input
+              type="text"
+              value={this.state.value}
+              onChange={this.handleChange}
+            ></input>
+ 
+            <button onClick={this.update}>Update</button>
+          </div>
         </div>
+      </>
+    )
+  }
 
-
-           </div>
-           
-           </>  
-        )
-    }
-        
-    
 }
 
 //code ralted to REDUX states
 
-
 const mapStateToProps = (state) => ({
-    count: state.counter.value
-  });
-  
-  const mapDispatchToProps = { addValue };
+  count: state.counter.value,
+})
+
+const mapDispatchToProps = { addValue }
 
 export default connect(mapStateToProps, mapDispatchToProps)(NavBar)

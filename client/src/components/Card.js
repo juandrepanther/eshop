c28cba5b9eb6075d837ae63d6c846245254b01cd
juandrepanther <janis@innergang.com>
2021-07-06 04:09:06 +0300

    currency Changer in Card component completed

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,30 +1,44 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-export default class Card extends Component {
-    render() {
-            return (
-              <>
-                <div className="products-container">
-                  <div className="products-card-wrapper">
-                    {this.props.data.map((product) => {
-                      return (
-                        <div className="card-container">
-                          <img
-                            className="card-image"
-                            alt=""
-                            src={product.gallery[0]}
-                          ></img>
-                          <div className="card-text-box">
-                            <h3>{product.name}</h3>
-                            <h3>set currency in Redux first</h3>
-                          </div>
-                        </div>
-                      )
-                    })}
+class Card extends Component {
+  render() {
+    const currency = this.props.currency //example USD 'string'
+    const currencyItem = ['USD', 'GBP', 'AUD', 'JPY', 'RUB']
+    const index = currencyItem.indexOf(currency)
+    const icons = ['$', '£', '$', '¥', '₽']
+
+    return (
+      <>
+        <div className="products-container">
+          <div className="products-card-wrapper">
+            {this.props.data.map((product) => {
+              return (
+                <div className="card-container">
+                  <img
+                    className="card-image"
+                    alt=""
+                    src={product.gallery[0]}
+                  ></img>
+                  <div className="card-text-box">
+                    <h3>{product.name}</h3>
+                    <h3>
+                      {`${icons[index]}`}
+                      {product.prices.map((i) => Object.values(i)[0])[index]}
+                    </h3>
                   </div>
                 </div>
-              </>
-            )
-          
-    }
+              )
+            })}
+          </div>
+        </div>
+      </>
+    )
+  }
 }
+
+const mapStateToProps = (state) => ({
+  currency: state.currency.currency,
+})
+
+export default connect(mapStateToProps)(Card)

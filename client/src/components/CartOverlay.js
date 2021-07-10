b5b9e8efe9a5f81c227879b1e3381275a895aac4
juandrepanther <janis@innergang.com>
2021-07-10 18:03:43 +0300

    created inc and dec counters, connected with redux store

--- client/src/components/CartOverlay.js
+++ client/src/components/CartOverlay.js
@@ -16,6 +16,7 @@ class CartOverlay extends Component {
     this.updateCounter = this.updateCounter.bind(this)
     this.getTotal = this.getTotal.bind(this)
     this.getPrice = this.getPrice.bind(this)
+    this.showCartComponent = this.showCartComponent.bind(this)
   }
 
   showCart() {
@@ -67,48 +68,73 @@ class CartOverlay extends Component {
     }
   }
 
-  render() {
+  showCartComponent() {
     const items = this.props.items
     const currency = this.props.currency //example USD 'string'
     const currencyItem = ['USD', 'GBP', 'AUD', 'JPY', 'RUB']
     const currencyIndex = currencyItem.indexOf(currency)
 
-    function Cart() {
-      return (
-        <div className="cart-container">
-          <div className="cart-wrapper">
-            <h5>CART</h5>
-            {items.map((item) => {
-              return (
-                <>
-                  <div className="cartoverlay-item" key={item.id}>
-                    <div className="cartoverlay-item-info">
-                      <p>{item.data.name}</p>
-                      {this.getPrice(item, currencyIndex)}
-                      <div className="cartoverlay-item-info-decisions-box-wrapper">
-                        {Object.values(item.decisions).map((decision) => {
-                          return (
-                            <>
-                              <div className="cartoverlay-item-info-decisions-box">
-                                {decision}
-                              </div>
-                            </>
-                          )
-                        })}
+    return (
+      <div className="cart-container">
+        {items.map((item) => {
+          return (
+            <div className="cart-wrapper">
+              <h5>CART</h5>
+              {items.map((item) => {
+                return (
+                  <>
+                    <div className="cartoverlay-item" key={item.id}>
+                      <div className="cartoverlay-item-info">
+                        <p>{item.data.name}</p>
+                        {this.getPrice(item, currencyIndex)}
+                        <div className="cartoverlay-item-info-decisions-box-wrapper">
+                          {Object.values(item.decisions).map((decision) => {
+                            return (
+                              <>
+                                <div className="cartoverlay-item-info-decisions-box">
+                                  {decision}
+                                </div>
+                              </>
+                            )
+                          })}
+                        </div>
+                      </div>
+                      <div className="cartoverlay-item-counter">
+                        <button
+                          onClick={() =>
+                            this.updateCounter(item.id, 'increment')
+                          }
+                        >
+                          +
+                        </button>
+                        <div>{item.count}</div>
+                        <button
+                          onClick={() =>
+                            this.updateCounter(item.id, 'decrement')
+                          }
+                        >
+                          -
+                        </button>
+                      </div>
+                      <div className="cartoverlay-item-image">
+                        <img alt="" src={item.data.gallery[0]} />
                       </div>
                     </div>
-                    <div className="cartoverlay-item-counter">Counter</div>
-                    <div className="cartoverlay-item-image">
-                      <img alt="" src={item.data.gallery[0]} />
-                    </div>
-                  </div>
-                </>
-              )
-            })}
-          </div>
-        </div>
-      )
-    }
+                  </>
+                )
+              })}
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
+
+  render() {
+    const items = this.props.items
+    const currency = this.props.currency //example USD 'string'
+    const currencyItem = ['USD', 'GBP', 'AUD', 'JPY', 'RUB']
+    const currencyIndex = currencyItem.indexOf(currency)
 
     return (
       <>
@@ -178,7 +204,7 @@ class CartOverlay extends Component {
             <button className="cartoverlay-checkout-checkBtn">CHECK OUT</button>
           </div>
         </div>
-        {this.state.show && <Cart />}
+        {this.state.show && this.showCartComponent()}
       </>
     )
   }

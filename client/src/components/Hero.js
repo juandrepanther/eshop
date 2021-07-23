177e7bade90062ee535711c0b322acf7f7355a26
juandrepanther <janis@innergang.com>
2021-07-23 15:45:15 +0300

    fixed routes on Hero component

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,51 +1,18 @@
 import { PureComponent } from 'react'
 import { Route } from 'react-router-dom'
-import { connect } from 'react-redux'
 import '../styles/Category.css'
 import Cards from './Cards'
 import MainCart from './MainCart'
-import { showPdp } from '../redux/showPdpReducer'
-
-//GraphQl querry and imports
-import { gql } from 'apollo-boost'
-import { Query } from 'react-apollo'
-
-const GET_CATEGORIES = gql`
- {
-  category {
-   products {
-    category
-   }
-  }
- }
-`
-
 class Hero extends PureComponent {
  render() {
   return (
    <>
     <Route exact path="/" component={Cards} />
-    <Query query={GET_CATEGORIES}>
-     {({ loading, error }) => {
-      if (loading) return <h4>Loading...</h4>
-      if (error) console.log(error)
-      return (
-       <>
-        <Route exact path="/:category" component={Cards} />
-       </>
-      )
-     }}
-    </Query>
+    <Route exact path="/:category" component={Cards} />
     <Route exact path="/cart" component={MainCart} />
    </>
   )
  }
 }
 
-//REDUX CODES
-const mapStateToProps = (state) => ({
- category: state.category,
-})
-const mapDispatchToProps = { showPdp }
-
-export default connect(mapStateToProps, mapDispatchToProps)(Hero)
+export default Hero
